895158baefa6cb952e107570c59e39c0c0683e40
Jason Tan <116934441+jasontanx@users.noreply.github.com>
2024-09-15 13:19:05 +0800

    requirements and config file

---
+++ conf.py
@@ -0,0 +1,10 @@
+'''
+BQ project details
+
+'''
+
+project_id = "myfirstproject-364809"
+dataset_id = "MY_FOREIGN_MONTHLY_ARR"
+table_id = "mas_monthly_foriegn_arr"
+key_path = "./key.json"
+filename = 'mas_monthly_foreign_arr.csv'
